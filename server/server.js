2ee49194dfaa4bca64ad64de803fab1eb82af2e4
Michael Bashford <mikebashford@gmail.com>
2022-03-22 17:56:35 -0500

    Implemented client side GraphQL

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,6 +26,14 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
